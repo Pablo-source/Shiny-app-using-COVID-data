bcac1b5c55f7feb4ad0d162b20189ff2c98370af
Pablo-source <datavizvalencia@gmail.com>
2024-07-22 11:11:36 +0100

    completed_initial_data_wrangling

--- 01_Shiny_app_data_prep.R
+++ 01_Shiny_app_data_prep.R
@@ -68,7 +68,8 @@ confirmed_tidy <- data_Confirmed %>%
                cols = 5:ncol(data_Confirmed)) %>% 
   group_by(Province,Country,Lat,Long,date) %>% 
   summarise("Confirmed"= sum(value,na.rm = T)) %>% 
-  mutate(date =as.Date(date,"%m/%d/%y"))
+  mutate(date =as.Date(date,"%m/%d/%y")) %>% 
+  ungroup()
 
 # 3.2 Creating DECEASED TIDY file. Pivot file from wide to long. 
 # DECEASED TIDY
@@ -79,7 +80,8 @@ deceased_tidy <- data_Deaths %>%
                cols = 5:ncol(data_Deaths)) %>% 
   group_by(Province,Country,Lat,Long,date) %>% 
   summarise("Deaths" = sum(value,na.rm = T)) %>% 
-  mutate(date = as.Date(date,"%m/%d/%y"))
+  mutate(date = as.Date(date,"%m/%d/%y")) %>% 
+  ungroup()
 
 # 3.3 Creating RECOVERED TIDY file. Pivot file from wide to long. 
 # RECOVERED TIDY
@@ -90,7 +92,8 @@ recovered_tidy <- data_Recovered %>%
                cols = 5:ncol(data_Recovered)) %>% 
   group_by(Province,Country,Lat,Long,date) %>% 
   summarise("Recovered" = sum(value,na.rm = T)) %>% 
-  mutate(date = as.Date(date,"%m/%d/%y"))
+  mutate(date = as.Date(date,"%m/%d/%y")) %>% 
+  ungroup()
 
 # 3.4 Save checks in new sub_folder called Checks using HERE package 
 if (!file.exists("Checks")) {dir.create("Checks")}
@@ -105,3 +108,72 @@ if (!file.exists("Shiny_howto_tutorials")) {dir.create("Shiny_howto_tutorials")}
 
 # Saved two .qmd files as templaets: - Mutating_Joins_dplyr.qmd, Pivot_long_wide_tidy.qmd
 
+# 4. Merge together Confirmed, Recovered and Deceased files now in LONG format 
+#  confirmed_tidy, deceased_tidy, recovered_tidy
+
+# 4.1 Merge Confirmed and Recovered files
+#     by(Province,Country,Lat,Long,date)
+
+confirmed_tidy_sub <- confirmed_tidy %>% select(Country,Lat,Long,date,Confirmed)
+recovered_tidy_sub <- recovered_tidy  %>% select(Country,Lat,Long,date,Recovered)
+deceased_tidy_sub <- deceased_tidy  %>% select(Country,Lat,Long,date,Deaths)
+
+
+conf_rec_join <-left_join(confirmed_tidy_sub,recovered_tidy_sub,
+                  by = join_by(Country == Country,
+                               Lat == Lat,
+                               Long == Long,
+                               date == date))
+conf_rec_join
+
+# 4.2 Merge previous two files with deceased_tidy file
+
+conf_rec_death_join <- left_join(conf_rec_join,deceased_tidy_sub,
+                                 by = join_by(Country == Country,
+                                              Lat == Lat,
+                                              Long == Long,
+                                              date == date))
+conf_rec_death_join
+
+write.csv(conf_rec_death_join,here("Checks","conf_rec_death_join.csv"), row.names = TRUE)
+
+# Now we have the intial three COVID-19 Metrics into a single file. 
+
+# 5. Start building DATASET used for a LEAFLET MAP
+# 5.1 First we ensure any null values are present in the data. If so we replace them by 0.
+conf_rec_death_join_WIDE <- conf_rec_death_join %>% 
+                    arrange(Country,date) %>% 
+                    # Recode NA values into 0 
+                    mutate(  
+                      Confirmed = ifelse(is.na(Confirmed),0,Confirmed),
+                      Deaths = ifelse(is.na(Deaths),0,Deaths),
+                      Recovered = ifelse(is.na(Recovered),0,Recovered)
+                    )
+conf_rec_death_join_WIDE
+
+# 5.2 We need to change the data structure to group Confirmed, Deaths, Recovered 
+#     into a single COLUMN with a variable displaying three values 
+#     We only need (Country, Lat, Long, date, Confirmed, Recovered, Deaths)
+MAPDATA_prep_long <- conf_rec_death_join_WIDE %>% 
+                      select(Country, Lat, Long, date, Confirmed, Recovered, Deaths) %>% 
+                      pivot_longer(names_to = "Metric",
+                                   cols = c("Confirmed","Deaths","Recovered")) %>% 
+                      ungroup()
+MAPDATA_prep_long
+
+
+# 6. Final two datasets creaetd
+
+# 6.1 File to be used in the LEAFLET map
+# Input file: MAPDATA_prep_long
+LEAFLET_MAPS_DATA <- MAPDATA_prep_long
+LEAFLET_MAPS_DATA
+
+# 6.2 File to be used in the RATES CALCULATION tables
+# Input file:conf_rec_death_join_WIDE
+METRICS_FOR_POP_RATES <- conf_rec_death_join_WIDE
+METRICS_FOR_POP_RATES
+
+# Save both data sets in the "original_data_processed" folder:
+write.csv(LEAFLET_MAPS_DATA,here("original_data_processed","LEAFLET_MAPS_DATA.csv"), row.names = TRUE)
+write.csv(METRICS_FOR_POP_RATES,here("original_data_processed","METRICS_FOR_POP_RATES.csv"), row.names = TRUE)
