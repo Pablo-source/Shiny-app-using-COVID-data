d0a69709a9184c3078d9b5363404d1fec2517c9c
Pablo-source <datavizvalencia@gmail.com>
2024-07-29 17:58:20 +0100

    reconciling METRICS POPULATION files

--- 02 Covid_metrics_population_rates.R
+++ 02 Covid_metrics_population_rates.R
@@ -39,56 +39,33 @@ nrow(METRICS_country_unique)
 write.csv(METRICS_country_unique,here("original_data_processed","METRICS_country_unique.csv"), row.names = TRUE)
 
 # 2. Load POPULATION figures from file  "WDI_countries_pop_2010_clean.csv" file
-population_data  <- list.files("original_data_processed/", pattern = "WDI_countries_pop_2019_.*\\.csv")
-population_data
+library(tidyverse)
+library(here)
+if (!require("WDI")) install.packages("WDI")
+library(WDI)
 
-file_Name <- c("WDI_countries_pop_2019_clean")
+# 1. Load  World Bank Total population by country indicator "SP.POP.TOTL" from {WDI} package
+WDI_population <- WDI(indicator = c("SP.POP.TOTL"), extra = TRUE)
+WDI_population
 
-for (name in file_Name) {
-  
-  match_name <- population_data[grepl(name, population_data)]
-  
-  if(length(match_name) > 0) {
-    assign(paste0("data_",name), read_csv(paste0("original_data_processed/",match_name)))
-  }
-}
+# 3. Filter values retaining Year 2019 - when pandemic started - to compute rates
+# Also remove null values for capital variable
+WDI_countries_pop_2019 <- WDI_population %>% 
+  select(country,year,population = SP.POP.TOTL,capital) %>% 
+  filter(year == 2019 &
+           capital != "")  # (capital ! = " ") Removed all instances when capital variable is emtpy.
+WDI_countries_pop_2019  
 
-write.csv(METRICS,here("data_","WDI_c.csv"), row.names = TRUE)
-
-POPULATION <-  data_WDI_countries_pop_2019_clean %>% 
-               select (Country = country, year, population)
-write.csv(POPULATION,here("original_data_processed","POPULATION.csv"), row.names = TRUE)
+head(WDI_countries_pop_2019)
 
 
-# Check unique country names from this POPULATION file
-POPULATION_country_unique <- POPULATION %>% select(Country) %>% distinct(Country)
-POPULATION_country_unique
-nrow(POPULATION_country_unique)
-write.csv(POPULATION_country_unique,here("original_data_processed","POPULATION_country_unique.csv"), row.names = TRUE)
+POPULATION_original <- WDI_countries_pop_2019 %>% select(Country = country,
+                                                         year,population)
+POPULATION_original
+head(POPULATION_original)
 
-POPULATION <-read.table(here("original_data_processed", "POPULATION_country_unique.csv"),
-                           header =TRUE, sep =',',stringsAsFactors =TRUE)
 
-# 3. Reconcile COUNTRY NAMES in both files METRICS and POPULATION
-
-# There are some Country names from the original METRICS file that do not have
-# similar match in the POPULATION file. 
-
-# To test it I will do a LEFT merge between METRICS and POPULATION files and 
-# check NA after the merge, which countries do not have available population figures
-# Then I will go back to WDI to obtain population figures for those countries and create a dataframe with those countries population
-# figures and merge it again with METRICS data frame until all countries have their population figures populated in the METRICS file
-
-# 3.1 Merge METRICS with POPULATION 
-
-# Ljoin <-left_join(Base,New,by = join_by(Brand == Marca))
-# Ljoin
-
-# names(METRICS)
-# [1] "Country"   "Lat"       "Long"      "date"      "Confirmed" "Recovered" "Deaths"  
-# names(POPULATION)
-# [1] "Country"    "year"       "population"
-
+# This is the merge we will perform 
 METRICS_POP_RATES_checks <- left_join(METRICS,POPULATION,
                                 by = join_by(Country == Country))
 METRICS_POP_RATES_checks
@@ -100,7 +77,7 @@ no_pop_countries <- METRICS_POP_RATES_checks %>%
 no_pop_countries
 write.csv(no_pop_countries,here("original_data_processed","countries_missing_population_figures.csv"), row.names = TRUE)
 
-
+# LIST OF COUNTRY NAMES FROM METRICS data frame that are slightly different written in the WDI_countries_pop_2019 file
 # List of missing population figures for the following mismatching country names
 # 1 Bahamas, The                            # 27 Slovakia
 # 2 Brunei                                  # 28 Syria
@@ -129,28 +106,7 @@ write.csv(no_pop_countries,here("original_data_processed","countries_missing_pop
 # 25 Saint Lucia
 # 26 Saint Vincent and the Grenadines
 
-# 4. Initial population MERGE (it has mismatching names)
-METRICS_POP_RATES_incomplete <- left_join(METRICS,POPULATION,
-                                      by = join_by(Country == Country))
-METRICS_POP_RATES_incomplete
+# 4. Re-code POPULATION_original into POPULATION file with the right country names that will match METRICS file 
 
-
-# 5. Populate missing countries population figures
-# For this list of 35 Countries, I will go back to the {WDI} package and create a new dataset with these 35 missing countries
-# When I will do a sc
-
-library(WDI)
-
-# 1. Load  World Bank Total population by country indicator "SP.POP.TOTL" from {WDI} package
-WDI_population <- WDI(indicator = c("SP.POP.TOTL"), extra = TRUE)
-WDI_population
-
-WDI_population_reconcile <-WDI_population  %>% 
-                            select(country,year,population = SP.POP.TOTL,capital) %>% 
-                            filter(year == 2019 &
-                            capital != "") 
-WDI_population_reconcile
-
-# 5.1 Check I am able to find each of the missing countries by doing a fizzy country name search:
 
 
