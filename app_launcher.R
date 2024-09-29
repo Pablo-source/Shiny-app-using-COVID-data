3a91a9e3d1a078808e6d1a99741550f8b7cc7219
Pablo Leon <datavizvlc@gmail.com>
2024-09-29 18:52:53 +0100

    Update app_launcher.R

--- app_launcher.R
+++ app_launcher.R
@@ -1,5 +1,5 @@
 ## Shiny_app launcher
 # File: app_launcher.R
 
-# Ad hoc function to source files from R sub-folder
+# Use {shiny} runApp() function to launch Shiny-app-using-COVID-data app. 
 runApp('COVID_19_Shiny_app.R')
