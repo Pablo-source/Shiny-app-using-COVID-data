4e20ad5a57300e4a0d27c57ae6d63d0ed4e3ed9d
Pablo Leon <datavizvlc@gmail.com>
2024-09-30 08:28:23 +0100

    Update app_launch_TRIGGER.R

--- app_launch_TRIGGER.R
+++ app_launch_TRIGGER.R
@@ -6,6 +6,16 @@
 library(renv)
 
 # 2. Load shiny and shinydashboard libraries
+
+# Following readme file we would have loaded rqeuired packages into R after executing renv::restore() command
+# renv::restore()
+# The following package(s) will be updated:
+# CRAN -----------------------------------------------------------------------
+# - base64enc           [* -> 0.1-3]
+# ...
+# - Installing shiny ...                          OK [linked from cache in 0.00029s]
+# - Installing shinydashboard ...                 OK [linked from cache in 0.00029s]
+
 library(shiny)
 library(shinydashboard)
 
